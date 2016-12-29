d61f1dfc208ebd240f1458278c8b0fb7cce114c5
Mochammad Rezza <reza.wikan.dito@gmail.com>
2016-12-29 17:56:57 +0800

    admin fix issued last

--- admin/home.php
+++ admin/home.php
@@ -71,7 +71,7 @@
                 </div>
                 <ul class="nav navbar-top-links navbar-right">
                     <li>
-                        <a href="#">
+                        <a href="logout.php">
                             <i class="fa fa-sign-out"></i> Log out
                         </a>
                     </li>
